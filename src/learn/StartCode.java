ed1cfcb052b4fd0edd30e547ba580d121afd872b
Pramod Thorat <pramodthorat410@gmail.com>
2025-07-28 21:50:10 +0530

    Sample code

---
+++ src/learn/StartCode.java
@@ -0,0 +1,9 @@
+package learn;
+
+import java.util.*;
+public class StartCode {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to code java FrameWorks..");
+    }
+}
