da61cfc081990d7f22c5307eca5e4259f53e3ac7
Pramod Thorat <pramodthorat410@gmail.com>
2025-07-28 22:21:39 +0530

    Method usage

--- src/learn/StartCode.java
+++ src/learn/StartCode.java
@@ -21,14 +21,39 @@ public class StartCode {
         names.add("Alex");
         names.add("Bob");
         names.add("Coyote");
+        names.add("Delta");
 
         System.out.println(names);
 
+        /*
         LinkedList list = new LinkedList();
         list.add(10);
         list.add("James");
         list.add(true);
         list.add(3.123);
         System.out.println(list);
+         */
+
+
+        names.remove("Delta"); // use to remove the object or element.
+
+        System.out.println(names);
+
+        //Size
+        System.out.println(names.size());
+
+        // Check item is there or not
+        // Types ka equals method call krta hai checking karne k liye.
+        System.out.println(names.contains("Bob"));
+
+        // Check for empty
+        System.out.println(names.isEmpty());
+
+        // Setting values..
+        // Set will remove the element at the given position and replace it with new field.
+         names.set(1,"Maverick");
+
+        //names.add(1,"Maverick");
+
     }
 }
